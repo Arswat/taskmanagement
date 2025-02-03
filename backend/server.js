9a56ca6eddb8d4751394d680916595d8a97cdb88
Arswat <86073218+Arswat@users.noreply.github.com>
2025-02-03 09:42:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+console.log("MongoDB URI:", process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log("MongoDB Connected"))
